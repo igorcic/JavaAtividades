c5d7a21b5052ec9eca25d8601347435d734c6298
igor bastos <igorbastoscic@gmail.com>
2022-07-04 22:30:00 -0300

    Criando as classes de entities para fazer as relações entre elas

--- src/entities/HourContract.java
+++ src/entities/HourContract.java
@@ -40,4 +40,7 @@ public class HourContract {
     public void setHours(Integer hours) {
         this.hours = hours;
     }
+    public double totalValue(){
+        return valuePerHour * hours;
+    }
 }
---
+++ src/entities/Worker.java
@@ -0,0 +1,86 @@
+package entities;
+
+import enums.WorkerLevel;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class Worker {
+
+    private String name;
+    private WorkerLevel level;
+    private Double baseSalary;
+
+    private Departament departament;
+    private List<HourContract> contracts = new ArrayList<>();
+
+
+    public Worker() {
+
+    }
+
+    public Worker(String name, WorkerLevel level, Double baseSalary, Departament departament) {
+        this.name = name;
+        this.level = level;
+        this.baseSalary = baseSalary;
+        this.departament = departament;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public WorkerLevel getLevel() {
+        return level;
+    }
+
+    public void setLevel(WorkerLevel level) {
+        this.level = level;
+    }
+
+    public Double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(Double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    public Departament getDepartament() {
+        return departament;
+    }
+
+    public void setDepartament(Departament departament) {
+        this.departament = departament;
+    }
+
+    public List<HourContract> getContracts() {
+        return contracts;
+    }
+
+    public void addContract(HourContract contract){
+        contracts.add(contract);
+    }
+
+    public void removeContract(HourContract contract){
+        contracts.remove(contract);
+    }
+
+    public double income(int year, int month){
+        double soma = baseSalary;
+        Calendar cal
+        for(HourContract c: contracts){
+            int c_year;
+            int c_month;
+            if(){
+
+            }
+        }
+    }
+
+}
